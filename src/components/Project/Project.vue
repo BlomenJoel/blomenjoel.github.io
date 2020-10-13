a7859e44cac21242697e74ad3eb1054171b6975e
joel <joel.blomen@dahl.se>
2020-10-13 14:31:32 +0000

    add image files

---
+++ src/components/Project/Project.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <project-start :project="project" />
+  </div>
+</template>
+
+<script>
+  import ProjectStart from "./ProjectStart.vue";
+  import { Projects } from "../../service/projectText.ts";
+  export default {
+    components: { ProjectStart },
+    props: {},
+    computed: {},
+    data() {
+      return {
+        project: Projects,
+      };
+    },
+    created() {
+      this.getProjectData();
+    },
+    methods: {
+      getProjectData() {
+        this.project = Projects.find(project => project.key === this.$route.params.key );
+      },
+    },
+  };
+</script>
+
+<style scoped></style>
