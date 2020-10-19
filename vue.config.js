99434c478f5e361bd8c528d4f3e87bff0188f5eb
joel <joel.blomen@dahl.se>
2020-10-19 14:47:15 +0000

    remove at import

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production" ? "/portfolio-not-live/" : "/",
-  resolve: {
-    alias: {
-      "@": path.resolve("src"),
-    },
-  },
 };
